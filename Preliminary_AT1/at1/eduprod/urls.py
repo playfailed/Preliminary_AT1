7a8f745e6f5ff62349c78c522ce32168d04f0345
Playfailed <playfailed13@gmail.com>
2024-03-29 14:00:30 +1100

    Structure 1.2.1

--- Preliminary_AT1/at1/eduprod/urls.py
+++ Preliminary_AT1/at1/eduprod/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('Factorise', views.Factorise, name='Factorise'),
+    path('Complete', views.Complete, name='Complete'),
+    path('Formula', views.Formula, name='Formula'),
+    path('Test', views.Test, name='Test'),
+    path('Quiz', views.Quiz, name='Quiz'),
+    path('Progress', views.Progress, name='Progress'),
 ]
--- Preliminary_AT1/at1/eduprod/views.py
+++ Preliminary_AT1/at1/eduprod/views.py
@@ -8,3 +8,21 @@ def index(request):
     questions = Question.objects.all()
     questions_json = serializers.serialize('json', questions)
     return render(request, 'eduprod/index.html', {'questions_json': questions_json})
+
+def Factorise(request):
+    return render(request, 'eduprod/Factorise.html')
+
+def Complete(request):
+    return render(request, 'eduprod/Complete.html')
+
+def Formula(request):
+    return render(request, 'eduprod/Formula.html')
+
+def Test(request):
+    return render(request, 'eduprod/Test.html')
+
+def Quiz(request):
+    return render(request, 'eduprod/Quiz.html')
+
+def Progress(request):
+    return render(request, "eduprod/Progress.html")
